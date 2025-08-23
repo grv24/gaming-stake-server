40dd1f22cda6004b064ea5c079e3d8d40cdff514
adkood <ashutoshnegi195@gmail.com>
2025-08-23 14:26:55 +0530

    added all casinoType

--- src/cron/CasinoCronJob.ts
+++ src/cron/CasinoCronJob.ts
@@ -1,7 +1,35 @@
 import cron from "node-cron";
 import { fetchAndUpdateCasinoOdds } from "../services/casino/CasinoService";
 
-const CASINO_TYPES = ["lucky5"]; 
+const CASINO_TYPES = [
+  "dt6",
+  "teen",
+  "poker",
+  "teen20",
+  "teen9",
+  "teen8",
+  "poker20",
+  "poker6",
+  "card32eu",
+  "war",
+  "aaa",
+  "abj",
+  "dt20",
+  "lucky7eu",
+  "dt202",
+  "teenmuf",
+  "joker20",
+  "poison20",
+  "joker1",
+  "teen20c",
+  "btable2",
+  "goal",
+  "ab4",
+  "lottcard",
+  "lucky5",
+  "baccarat2"
+];
+
 
 export const startCasinoCronJobs = () => {
   CASINO_TYPES.forEach((casinoType) => {
