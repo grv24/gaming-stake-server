ed1e48fd7b63c4c92d1e2f6b9989ba42602a5662
adkood <ashutoshnegi195@gmail.com>
2025-08-23 15:27:00 +0530

    entity added to appdatasource

--- src/server.ts
+++ src/server.ts
@@ -26,6 +26,7 @@ import { initRedisPubSub } from './config/redisPubSub';
 import { AccountTrasaction } from './entities/Transactions/AccountTransactions';
 import { DefaultCasino } from './entities/casino/DefaultCasino';
 import { CasinoMatch } from './entities/casino/CasinoMatch';
+import { CasinoBet } from './entities/casino/CasinoBet';
 
 dotenv.config();
 
@@ -56,7 +57,8 @@ export const AppDataSource = new DataSource({
         MatkaSettings,
         AccountTrasaction,
         DefaultCasino,
-        CasinoMatch
+        CasinoMatch,
+        CasinoBet
     ],
     synchronize: true,
     logging: process.env.NODE_ENV === 'development',
